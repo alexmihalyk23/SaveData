b4f4da521d9506c38faf76e12de9d2889f51a219
alexmihalyk <alexmihalyk23@gmail.com>
2021-10-15 05:41:17 +0700

    Initial commit

---
+++ app/src/main/java/com/example/savedata/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.savedata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    String name ="неопределено";
+    final static String nameVariableKey = "NAME_VAR";
+    final static String textViewTexKey = "TEXT_VIEW";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        outState.putString(nameVariableKey, name);
+        TextView nameView = (TextView) findViewById(R.id.textView);
+        outState.putString(textViewTexKey, nameView.getText().toString());
+        super.onSaveInstanceState(outState);
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState)
+    {
+        super.onRestoreInstanceState(savedInstanceState);
+        name = savedInstanceState.getString(nameVariableKey);
+        String textViewText= savedInstanceState.getString(textViewTexKey);
+        TextView nameView = (TextView) findViewById(R.id.textView);
+        nameView.setText(textViewText);
+    }
+    public void restoreField(View view)
+    {
+        TextView nameView = (TextView) findViewById(R.id.textView);
+        nameView.setText(name);
+    }
+    public void saveField(View view)
+    {
+        TextView nameBox = (TextView) findViewById(R.id.plain_text_input);
+        name = nameBox.getText().toString();
+    }
+}
